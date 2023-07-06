3a400e2d10a30e3e3987d6252bdd90b456ff3c1a
Jeremy_1806 <2019117@iiitdmj.ac.in>
2023-07-06 12:42:35 +0530

    added Uploads Model and tested file saving

--- pdfmanagement_be/uploads/models.py
+++ pdfmanagement_be/uploads/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Upload(models.Model):
+    file_id = models.AutoField(primary_key=True, unique=True,null=False)
+    uploaded_file = models.BinaryField(verbose_name='Uploaded File', blank=False)
+    uploaded_by = models.EmailField(null=False,unique=False)
+    uploaded_at = models.DateTimeField(null=False, default=datetime.now)
--- pdfmanagement_be/uploads/views.py
+++ pdfmanagement_be/uploads/views.py
@@ -1,9 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.parsers import MultiPartParser, FileUploadParser, FormParser
+from uploads.models import Upload
 
 @api_view(['POST'])
 def upload_file(request, **kwargs):
+    parser_classes = [MultiPartParser, FileUploadParser, FormParser]
+    file = request.FILES['uploaded_file']
+    file= file.read()
+    print(type(file))
+    dummy_email = "trialupload@email.com"
+
+    op = Upload.objects.create(uploaded_by = dummy_email , uploaded_file = file)
     return Response("Working upload")
 
 
