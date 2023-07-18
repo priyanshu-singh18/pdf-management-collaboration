f6492cad376bdcf9b51d76fe7fc3514ee9ba89a7
Jeremy_1806 <2019117@iiitdmj.ac.in>
2023-07-18 16:01:07 +0530

    modified build file

--- pdfmanagement_be/build.sh
+++ pdfmanagement_be/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
+pip install -r requirements.txt
+
 set -o errexit
 
 poetry install
 pip install --upgrade pippip install --force-reinstall -U setuptools
-
+python manage.py makemigrations
 python manage.py migrate
